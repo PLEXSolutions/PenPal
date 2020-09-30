57a9fb7df45bfd71f40750b21986b1093b505290
Joshua Christman <josh.christman@plex-llc.com>
2020-09-30 13:41:40 -0500

    - Quick bug fix on improper startup of n8n

--- Server/src/server/n8n.js
+++ Server/src/server/n8n.js
@@ -12,8 +12,6 @@ const generateNodes = async () => {
     const { settings: { n8n } = {} } = PenPal.LoadedPlugins[key];
     if (n8n === undefined) continue;
 
-    const output_dir = path.join(N8N_NODES_DIR, n8n.name);
-
     const output_ts_file = path.join(
       N8N_NODES_DIR,
       n8n.name,
@@ -28,9 +26,7 @@ const generateNodes = async () => {
 
     try {
       await fs.mkdir(output_dir);
-    } catch (e) {
-      console.error(e);
-    }
+    } catch (e) {}
 
     console.log(`[.] Generating n8n node ${n8n.name}`);
 
@@ -69,13 +65,10 @@ const buildNodes = () => {
 const killOldServer = () => {
   console.log();
   console.log("[.] Killing old n8n server if still running");
-  spawnSync(
-    `ps -ef | grep "npm run start" | awk '{print $2}' | xargs kill -9`,
-    {
-      stdio: "ignore",
-      shell: true
-    }
-  );
+  spawnSync(`ps -ef | grep "n8n" | awk '{print $2}' | xargs kill -9`, {
+    stdio: "ignore",
+    shell: true
+  });
 };
 
 const startN8nServer = () => {
@@ -83,15 +76,14 @@ const startN8nServer = () => {
   console.log();
   spawn("npm run start", {
     stdio: "inherit",
-    shell: "true",
-    cwd: N8N_DIR,
-    detached: true
+    shell: true,
+    cwd: N8N_DIR
   });
 };
 
 export default async () => {
+  killOldServer();
   await generateNodes();
   buildNodes();
-  killOldServer();
   startN8nServer();
 };
