1d94319620a7ef2d55a3709f8394099c2f5b1b06
Joshua Christman <josh.christman@plex-llc.com>
2021-01-06 11:41:31 -0600

    - Modified to work with Apollo client v3. Fixes dashboard problems

--- Server/src/client/modules/components/pages/dashboard.js
+++ Server/src/client/modules/components/pages/dashboard.js
@@ -65,7 +65,7 @@ const Dashboard = () => {
         }
       }
     })();
-  }, [loading, getDashboardablePlugins.length]);
+  }, [loading, query]);
 
   // ---------------------- Hooks ---------------------- //
 
