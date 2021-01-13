f31769ffe15eaa6181077a52cfe14bdf53fc8a40
Joshua Christman <josh.christman@plex-llc.com>
2021-01-13 14:01:47 -0600

    - Updated documentation - Converted Masscan dockerfile to an exported string from Masscan

--- Plugins/Masscan/server/plugin.js
+++ Plugins/Masscan/server/plugin.js
@@ -1,36 +1,10 @@
 import { types, resolvers, loaders } from "./graphql";
+import dockerfile from './Dockerfile.js';
 
 const settings = {
   docker: {
     name: "masscan",
-    dockerfile: `
-FROM ubuntu:14.04
-
-# Set locales
-RUN locale-gen en_GB.UTF-8
-ENV LANG en_GB.UTF-8
-ENV LC_CTYPE en_GB.UTF-8
-
-# Fix sh
-RUN rm /bin/sh && ln -s /bin/bash /bin/sh
-
-# Install dependencies
-RUN apt-get update
-RUN apt-get install -y git build-essential curl wget libpcap-dev
-
-# Clone masscan git repo
-RUN git clone https://github.com/robertdavidgraham/masscan /opt/masscan
-WORKDIR /opt/masscan
-
-# Make masscan
-RUN make -j
-
-# Copy binary
-RUN cp /opt/masscan/bin/masscan /usr/local/bin
-
-# Launch Bash
-CMD ["/bin/bash"]
-  `,
+    dockerfile,
   },
   n8n: {
     workflow_nodes: [
